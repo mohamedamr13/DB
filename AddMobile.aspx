9ee374cc6e1c3c1a982f2910fdc6b6adc313585f
Nerimane107 <68298009+Nerimane107@users.noreply.github.com>
2021-01-08 04:09:49 +0200

    Add files via upload
    added register , login and addMobile .
    AddMobile is not the same as MobileNumber. nafs el functionality bas kont bagarbha f webform gedida

---
+++ AddMobile.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddMobile.aspx.cs" Inherits="Gucera.AddMobile" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            position: absolute;
+            top: 49px;
+            left: 246px;
+            z-index: 1;
+            bottom: 208px;
+            right: 843px;
+        }
+        .auto-style2 {
+            position: absolute;
+            top: 53px;
+            left: 16px;
+            z-index: 1;
+            width: 168px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            please add your mobile number(s):<br />
+&nbsp;<asp:TextBox ID="mobile" runat="server" CssClass="auto-style2" TextMode="Phone"></asp:TextBox>
+        </div>
+        <asp:Button ID="add" runat="server" CssClass="auto-style1"  onClick="onSave" Text="Save" Height="20px" />
+    </form>
+</body>
+</html>
---
+++ AddMobile.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gucera
+{
+    public partial class AddMobile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void onSave(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["guc"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+            String phone = mobile.Text;
+            SqlCommand addMobileProc = new SqlCommand("addMobile", conn);
+            addMobileProc.CommandType = CommandType.StoredProcedure;
+            addMobileProc.Parameters.Add(new SqlParameter("@ID", Session["user"]));
+            addMobileProc.Parameters.Add(new SqlParameter("@mobile_number", phone));
+            conn.Open();
+            addMobileProc.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+    }
+}
